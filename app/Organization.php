2989f3f2b4fe19648c6783fed3fadce5542ea88c
Pietro <pietro@fixico.nl>
2018-03-10 00:26:00 +0100

    Basic migration to create organization table, skeleton for organization model

---
+++ app/Organization.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Api;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'organizations';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = [];
+
+
+}
---
+++ database/migrations/2018_03_10_001222_create_organization_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $t) {
+            $t->increments('id');
+            $t->string('reference_code')->nullable();
+            $t->string('name');
+            $t->string('email');
+            $t->string('phone_number');
+            $t->string('website');
+            $t->string('type');
+            $t->float('lat');
+            $t->float('long');
+
+            $t->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
